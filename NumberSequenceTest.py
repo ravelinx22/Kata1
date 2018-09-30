7a52120d6992b000c8174168a69a093ee49bfa6e
William Ravelo <drummerwilliam@gmail.com>
2018-09-30 12:21:02 -0500

    Iteracion 2 - Se agrego test para probar caso con N numero (Rojo)

--- NumberSequenceTest.py
+++ NumberSequenceTest.py
@@ -32,3 +32,7 @@ class NumberSequenceTest(TestCase):
     def test_elementMaxMinI2DosNumero(self):
         ans = NumberSequence().elementMaxMin("0,1")
         self.assertEqual(ans[1], 0, "2 numero")
+
+    def test_elementMaxMinI2NNumero(self):
+        ans = NumberSequence().elementMaxMin("4,5,6,7,8,9,10,11,12")
+        self.assertEqual(ans[1], 4, "N numeros")
