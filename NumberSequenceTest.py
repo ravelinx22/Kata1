37797e828e25de9fcd1d586c2f4105c43519a145
William Ravelo <drummerwilliam@gmail.com>
2018-09-30 11:40:33 -0500

    Iteracion 1 - Se agrego test para probar caso con un numero (Rojo)

--- NumberSequenceTest.py
+++ NumberSequenceTest.py
@@ -3,5 +3,8 @@ from NumberSequence import NumberSequence
 
 
 class NumberSequenceTest(TestCase):
-    def test_elementMaxMin(self):
+    def test_elementMaxMinI1(self):
         self.assertEqual(NumberSequence().elementMaxMin(""),[0], "Cadena Vacia")
+
+    def test_elementMaxMinI1UnNumero(self):
+        self.assertEqual(NumberSequence.elementMaxMin("1"), [1], "1 numero")
