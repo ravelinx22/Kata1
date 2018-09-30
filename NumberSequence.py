c25f8faba2f2d1f38cbd06c666c2c0da90b37a90
Diego Rodríguez <diegorbaquero@gmail.com>
2018-09-30 12:31:50 -0500

    Iteración 3 - Agregar código para vacío (verde)

--- NumberSequence.py
+++ NumberSequence.py
@@ -1,7 +1,7 @@
 class NumberSequence:
     def elementMaxMin(self, cadena):
         if cadena == "":
-            return [0, None]
+            return [0, None, None]
         else:
             nums = list(map(int, cadena.split(",")))
             return [len(nums), min(nums)]
