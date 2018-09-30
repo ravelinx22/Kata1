a9ce51fa66efcc4a24b83b4bb5e97f015c8e98e8
Diego Rodríguez <diegorbaquero@gmail.com>
2018-09-30 11:48:28 -0500

    Iteración 1 - Agregar código para 2 número (Verde)

--- NumberSequence.py
+++ NumberSequence.py
@@ -2,5 +2,7 @@ class NumberSequence:
     def elementMaxMin(self, cadena):
         if cadena == "":
             return [0]
+        elif len(cadena) > 1:
+            return [2]
         else:
             return [1]
