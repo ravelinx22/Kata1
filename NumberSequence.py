08880e5596dc34d896257d6a334c73a04ae34317
Diego Rodríguez <diegorbaquero@gmail.com>
2018-09-30 19:30:27 -0500

    Iteración 4 - Agregar código cadena vacía (verde)

--- NumberSequence.py
+++ NumberSequence.py
@@ -1,7 +1,7 @@
 class NumberSequence:
     def elementMaxMin(self, cadena):
         if cadena == "":
-            return [0, None, None]
+            return [0, None, None, None]
         else:
             nums = list(map(int, cadena.split(",")))
             return [len(nums), min(nums), max(nums)]
