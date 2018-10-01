05a3c46b95c2ec5bb91b145584eac00e03985fd0
Diego Rodríguez <diegorbaquero@gmail.com>
2018-09-30 19:42:12 -0500

    Iteración 4 - Agregar código n nums (verde)

--- NumberSequence.py
+++ NumberSequence.py
@@ -4,7 +4,4 @@ class NumberSequence:
             return [0, None, None, None]
         else:
             nums = list(map(int, cadena.split(",")))
-            if len(nums) == 1:
-                return [len(nums), min(nums), max(nums), 1]
-            else:
-                return [len(nums), min(nums), max(nums), sum(nums)/len(nums)]
+            return [len(nums), min(nums), max(nums), sum(nums)/len(nums)]
